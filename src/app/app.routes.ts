449cd18ac39146644804e5a3261c5548161ac425
Andrea <guerreroaanathalia@gmail.com>
2026-03-14 18:49:03 -0500

    instalacion de tailwind,tanstackquery, maquetacion basica de list e issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'issue/:number',
+    loadComponent: () => import('../app/modules/issues/pages/issue/issue-page'),
+  },
+  {
+    path: 'issues',
+    loadComponent: () => import('../app/modules/issues/pages/issues-list/issues-list-page'),
+  },
+  { 
+    path:'**',
+    redirectTo: 'issues'
+  }
+
+];
