eec74c343a3acf64ef6b61306ce39981186ed433
Andrea <guerreroaanathalia@gmail.com>
2026-03-15 22:08:04 -0500

    test con mock de fetch

---
+++ src/app/modules/issues/actions/get-issues-by-number.actions.spec.ts
@@ -0,0 +1,42 @@
+import { environment } from 'src/environments/environment.development';
+import { getIssueByNumber } from './get-issues-by-number.actions';
+
+const mockIssue = {
+  id: 1,
+  number: 123,
+  title: 'Test issue',
+  body: 'Test Body',
+};
+
+const BASE_URL = `${environment.baseUrl}/issues`;
+const GITHUB_TOKEN = environment.gitHUbToken;
+
+describe('getIssueByNumber', () => {
+  const mockIssueNumber = '123';
+  let originalFetch: typeof window.fetch;
+
+  beforeEach(() => {
+    originalFetch = window.fetch; //backup valor originalk
+  });
+  afterEach(() => {
+    window.fetch = originalFetch; //restablecer para otras pruebas
+  });
+
+  it('should fetch and return an issue successfully', async () => {
+    // console.log(window.fetch)
+
+    window.fetch = vi.fn().mockResolvedValue({
+      //llamada simulada a la api mock del fetch
+      ok: true,
+      json: () => Promise.resolve(mockIssue),
+    }); // siempre que se llame la api va a responder esto
+    const result = await getIssueByNumber(mockIssueNumber);
+    // console.log({result})
+    expect(window.fetch).toHaveBeenCalledWith(`${BASE_URL}/${mockIssueNumber}`, {
+      headers: {
+        Authorization: `Bearer${GITHUB_TOKEN}`,
+      },
+    });
+    expect(result).toEqual(mockIssue);
+  });
+});
